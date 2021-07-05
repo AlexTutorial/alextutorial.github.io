c3e2a2992bf8839c8379ba1b88683b7c0b7653e6
AlexTutorial <78077438+AlexTutorial@users.noreply.github.com>
2021-07-05 14:14:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = '1734369637:AAFtguRVj0jNZuMewRQ8w-A8yD_uwjWilbo'
+TOKEN = '****************************'
 ANSWERS = {1:4, 2:3, 3:1, 4:2, 5:1}
 BALLS = {1:2, 2:4, 3:1, 4:8, 5:25}
 QUESTIONS = {1:'Question 1 text', 2:'Question 2 text', 3:'Question 3 text', 4:'Question 4 text', 5:'Question 5 text'}
